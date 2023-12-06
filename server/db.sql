6376bd487f4debf2e4521bf83a356b35173254a7
Blade1802 <ayugaurpune@gmail.com>
2023-12-06 23:29:07 +0000

    Postgres connection setup

---
+++ server/db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE student_hub_v1;
+-- Users Table
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_type VARCHAR(10) NOT NULL check(user_type IN ('admin', 'student'))
+);
+INSERT INTO users (user_email, user_password, user_type)
+VALUES ('aayush', 'aayush123', 'admin');
+INSERT INTO users (user_email, user_password, user_type)
+VALUES ('test', 'test123', 'student');
